6b8ceef55c79b20839628adb4762f9c6744f8cea
Rahulkumar Gayatri <rgayatri@lbl.gov>
2022-01-27 13:30:45 -0800

    Adding separate tests for CUDA and OpenMP backends of Kokkos.

--- buildspecs/apps/kokkos/run-spack-kokkos-OpenMP-test.sh
+++ buildspecs/apps/kokkos/run-spack-kokkos-OpenMP-test.sh
@@ -13,14 +13,15 @@ export HOME_BASE=$(pwd)
 ml e4s/21.11-tcl
 ml kokkos/3.4.01-gcc-9.3.0
 
-if [ ! -d build ]; then
-    mkdir build 
+if [ ! -d build_OpenMP ]; then
+    mkdir build_OpenMP
 fi
-cd build
+cd build_OpenMP
 rm -rf *
 
 # Build the test 
 cmake \
+    -DCMAKE_CXX_EXTENSIONS=Off \
     -DCMAKE_CXX_COMPILER=g++ \
     -DKokkos_ROOT=$KOKKOS_ROOT \
     ${HOME_BASE}
