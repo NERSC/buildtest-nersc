2786138283822607739bc252d70eca4668a3545b
Rahulkumar Gayatri <rgayatri@lbl.gov>
2022-01-26 18:23:01 -0800

    Adding kokkos test script.

---
+++ buildspecs/apps/kokkos/run-spack-kokkos-test.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -xe
+
+# Load the right modules
+module load PrgEnv-gnu
+module load cmake
+module load cudatoolkit
+
+export HOME_BASE=$(pwd)
+
+# Load the e4s and kokkos modules
+ml e4s/21.11-tcl
+ml kokkos/3.4.01-gcc-9.3.0
+
+if [ ! -d build ]; then
+    mkdir build 
+fi
+cd build
+rm -rf *
+
+# Build the test 
+cmake \
+    -DCMAKE_CXX_COMPILER=g++ \
+    -DKokkos_ROOT=$KOKKOS_ROOT \
+    ${HOME_BASE}
+make -j64
+
+# Run the test
+./test
