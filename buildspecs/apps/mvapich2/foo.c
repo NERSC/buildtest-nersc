8874aa0cc3c98919ffca80d75c74ed0cdc3465c4
Shahzeb Siddiqui <shahzebmsiddiqui@gmail.com>
2022-07-27 07:39:51 -0700

    fix mvapich2 test

--- buildspecs/apps/mvapich2/foo.c
+++ buildspecs/apps/mvapich2/foo.c
@@ -1,11 +1,15 @@
-#include <mpi.h>
-int main(int argc, char **argv)
-{
-  int rank, size;
-  MPI_Init(&argc, &argv); 
-  MPI_Comm_size(MPI_COMM_WORLD, &size);
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  // Print off a hello world message
-  printf("Hello world from processor %d out of %d processors\n", rank, size);
-  MPI_Finalize();
-}
+#include "mpi.h" 
+#include <stdio.h> 
+int main(int argc, char *argv[]) 
+{ 
+    int rank, nprocs, len;
+    char name[MPI_MAX_PROCESSOR_NAME];
+
+    MPI_Init(&argc,&argv); 
+    MPI_Comm_size(MPI_COMM_WORLD,&nprocs); 
+    MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+    MPI_Get_processor_name(name, &len);
+    printf("Hello, world.  I am %d of %d on %s\n", rank, nprocs, name);fflush(stdout); 
+    MPI_Finalize(); 
+    return 0; 
+} 
