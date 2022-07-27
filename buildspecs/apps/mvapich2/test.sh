e4ba04d5ec180833092136cfe9fa54e0a86e1da6
Shahzeb Siddiqui <shahzebmsiddiqui@gmail.com>
2022-07-27 07:39:51 -0700

    fix mvapich2 test

--- buildspecs/apps/mvapich2/test.sh
+++ buildspecs/apps/mvapich2/test.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 #SBATCH --nodes 2
-#SBATCH -q regular 
-#SBATCH -t 60
+#SBATCH -q debug
+#SBATCH -t 5
 #SBATCH -C gpu
-#SBATCH -G 2 
-#SBATCH --account=nstaff_g
+#SBATCH -G 4 
+#SBATCH --account=m3503_g
 
 module use /global/cfs/cdirs/m3896/shared/modulefiles
 module load mvapich2/2.3.7
+nvidia-smi
 mpicc foo.c -o foo
 srun -n 2 ./foo
 
