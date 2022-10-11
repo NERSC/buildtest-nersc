638bc1660fbcf4c1478a8f966e2d9987a98addbb
Wyatt Spear <wspear@cs.uoregon.edu>
2022-10-11 13:14:03 -0700

    Added darshan tests to package list. Improved error checking and file handling

--- buildspecs/e4s/testgen/testgen.py
+++ buildspecs/e4s/testgen/testgen.py
@@ -13,23 +13,25 @@ package_path = sys.argv[2]
  
 #check if file is present
 if not os.path.isfile(file_path):
-    print("Provide a valid template file. "+file_path+" not found.")
-    exit()
+    exit("ERROR: Provide a valid template file. "+file_path+" not found.")
 
 if not os.path.isfile(package_path):
-    print("Provide a valid package-list.yml file. "+package_path+" not found.")
-    exit()
+    exit("ERROR: Provide a valid package-list.yml file. "+package_path+" not found.")
 
 packages=[]
 with open(package_path, 'r') as stream:
     packages = yaml.safe_load(stream)
 
-text_file = open(file_path, "r")
- 
-template = text_file.read()
-text_file.close()
+if packages is None or "package" not in packages:
+    exit("Error: No package entry found in "+package_path)
+
+template=""
+with  open(file_path, "r") as text_file:
+    template = text_file.read()
+
+if template is None or not template.strip():
+    exit("ERROR: Empty template file. "+file_path)
 
 for pname in packages["package"]:
-    ymlOut = open(pname+".yml", "w")
-    ymlOut.write(template.format(package=pname))
-    ymlOut.close()
+    with open(pname+".yml", "w") as ymlOut:
+        ymlOut.write(template.format(package=pname))
