bfb4475643c84780df948103f1afc6d224f88585
Shahzeb Siddiqui <shahzebmsiddiqui@gmail.com>
2023-03-28 09:14:12 -0700

    filter by module 'e4s' in lmodule test and update the name of test. add 'e4s' tagname in the test

--- buildspecs/modules/moduletest_perlmutter.py
+++ buildspecs/modules/moduletest_perlmutter.py
@@ -1,5 +1,5 @@
 from lmod.moduleloadtest import ModuleLoadTest
-test = ModuleLoadTest(debug=True, login=True)
+test = ModuleLoadTest(debug=True, login=True, name=['e4s'])
 results = test.get_results()
 print(results)
 assert(results['failed'] == 0)
