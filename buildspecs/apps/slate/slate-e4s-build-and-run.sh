a509441395c67fdd99250bc02be6d7b52edd402a
Asim YarKhan <yarkhan@icl.utk.edu>
2023-06-23 16:14:01 -0700

    For the script, make lapackpp path generic; use spack activate env gcc; echo paths to libraries for the record

--- buildspecs/apps/slate/slate-e4s-build-and-run.sh
+++ buildspecs/apps/slate/slate-e4s-build-and-run.sh
@@ -1,14 +1,19 @@
 ## Small shell script that builds and runs SLATE examples
 
-## salloc -A m3917 -C gpu -q regular -t 0:10:00 -N 1 --ntasks-per-node=4
+## salloc -A xxxxx -C gpu -q regular -t 0:20:00 -N 1 --ntasks-per-node=4
 ## ["-N 1", "-n 8", "-t 0:10:00"]
+## bash slate-e4s-build-and-run.sh
 
 module load spack/e4s-22.05
-spack env activate -V e4s
+spack env activate gcc
 spack load slate lapackpp blaspp
 export SLATE_DIR=$(spack location -i slate~cuda)
 export BLASPP_DIR=$(spack location -i blaspp~cuda)
-export LAPACKPP_DIR=$(spack location -i /jsh2p3h)
+export LAPACKPP_DIR=$(spack location -i lapackpp)
+
+echo $SLATE_DIR
+echo $BLASPP_DIR
+echo $LAPACKPP_DIR
 
 echo "Compiling SLATE examples using CMake in build subdir"
 cd examples/
